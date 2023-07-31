6e3d34fd1d485c829c58e5ec2bb19bcd0cd5ac31
gritchin-artur <123599698+gritchin-artur@users.noreply.github.com>
2023-07-31 20:43:36 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,8 @@ export const App = () => {
         alignItems: 'center',
         fontSize: 40,
         color: '#010101'
-      }}>
+      }}
+    >
       React homework template
     </div>
   );
